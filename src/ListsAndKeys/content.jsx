b64edcc955f1f059ccb6491cc6a0ba9aa635b7b3
Volodymyr Tsurko <sonicpro@ukrpost.ua>
2017-10-28 20:42:28 +0300

    A comment was corrected.

--- src/ListsAndKeys/content.jsx
+++ src/ListsAndKeys/content.jsx
@@ -1,4 +1,4 @@
-// A component that renders props.posts collection that a number of divs with a heading and a paragraph.
+// A component that renders props.posts collection as a number of divs with a heading and a paragraph.
 // No "root" element is rendered, so this control is supposed to be used as a part of some hierarchy.
 function Content(props) {
   // ASI problem is only applicable to the React elements following the return statement.
