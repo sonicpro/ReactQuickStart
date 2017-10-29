67517069e9c583413c12c9e30d5e2150608d6139
Volodymyr Tsurko <sonicpro@ukrpost.ua>
2017-10-29 10:18:02 +0200

    "Controlled Component" from "Forms" section demo added.

---
+++ src/Forms/controlledComponent.jsx
@@ -0,0 +1,37 @@
+// A component that controls the submission of a form with a single input.
+class Form extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { value: '' };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({
+      value: event.target.value
+    });
+  }
+
+  handleSubmit(event) {
+    alert('A name was submitted: ' + this.state.value);
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        {/* Notice htmlFor which is substitute for "for" in JSX. */}
+        <label htmlFor="input1">
+          Name:
+        </label>
+        <input id="input1"
+          type="text"
+          value={this.state.value}
+          onChange={this.handleChange}
+        />
+        <input type="submit" value="Submit" />
+      </form>
+    );
+  }
+}
